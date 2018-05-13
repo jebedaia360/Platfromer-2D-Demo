3d78dfdb15caac8b536334466403c233ec24b29e
Jeremi Biernacki <jeremi360@gmail.com>
2018-05-13 05:58:57 +0200

    Platfromer 2D odc. 4 : TitleMap

--- Platfromer 2D/Player.gd
+++ Platfromer 2D/Player.gd
@@ -1,32 +1,31 @@
 extends RigidBody2D
 
-var max_speed = 300
-var acc = 600 
-var jump_high = 400
-var curr_speed = 0 
+export var max_speed = 300
+export var acc = 600
+export var jump_high = 400
+var curr_speed = 0
 onready var ray = $RayCast2D
 
 func _ready():
 	$AnimationTreePlayer.active = true
 
 func _process(delta):
+	var x = abs(clamp(linear_velocity.x, -1, 1))
+	$AnimationTreePlayer.blend2_node_set_amount("idle-run", x)
+		
+	var y = floor(clamp(linear_velocity.y, -100, 100))/100
+	$AnimationTreePlayer.blend3_node_set_amount("air", y)
+	
+	if y > 0 and ray.is_colliding():
+		$AnimationTreePlayer.oneshot_node_start("land")
+	
 	if curr_speed > 0:
 		$BodyParts.scale.x = 1
 	if curr_speed < 0:
 		$BodyParts.scale.x = -1
-	
-	if ray.is_colliding():
-		$AnimationTreePlayer.blend2_node_set_amount(
-			"idle-run",
-			abs(curr_speed / max_speed)
-		)
 		
-		if Input.is_action_just_pressed("ui_up"):
-			$AnimationTreePlayer.oneshot_node_start("jump")
-		
-	else:
-		$AnimationTreePlayer.oneshot_node_start("fall")
-
+	if y == 1 and $BodyParts.scale.x == -1:
+		$BodyParts.rotation *= -1
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
@@ -56,10 +55,6 @@ func _physics_process(delta):
 			set_axis_velocity(
 				Vector2(curr_speed, 0)
 			)
-		
-		else:
-			var vy = linear_velocity.y
-			linear_velocity = Vector2(curr_speed, vy)
 	
 	if Input.is_action_pressed("ui_up"):
 		if ray.is_colliding():
