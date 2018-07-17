0eef826aa560cca478fa157ff63afa4a94735b6f
jeremi360 <jeremi360@gmail.com>
2018-07-17 13:52:15 +0200

    Platfromer 2D odc. 07 Timer, Singals, Setget

--- Platfromer 2D/CollectedLabel.gd
+++ Platfromer 2D/CollectedLabel.gd
@@ -3,10 +3,21 @@ extends Label
 var collected = 0
 var to_collect = 0
 
-func _process(delta):
+func _ready():
+	$Timer.connect("timeout", self, "on_timeout")
+	globals.connect("collected_changed", self, "on_collected_changed")
+
+func update_label():
+	text = str(collected) + "/" + str(to_collect)
+
+func on_timeout():
 	to_collect = globals.to_collect
 	collected = globals.collected
-	text = str(collected) + "/" + str(to_collect)
+	update_label()
+
+func on_collected_changed():
+	collected = globals.collected
+	update_label()
 
 
 
--- Platfromer 2D/globals.gd
+++ Platfromer 2D/globals.gd
@@ -1,5 +1,16 @@
 extends Node
 
-var collected = 0
+var collected = 0 setget set_collected, get_collected
 var to_collect = 0
+
+signal collected_changed
+
+func set_collected(value):
+	collected = value
+	emit_signal("collected_changed")
+
+func get_collected():
+	return collected
+	
+	
 
