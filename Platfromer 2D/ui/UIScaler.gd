c3e5e17715885667d520980433195de79117ff75
Jeremi Biernacki <jeremi360@gmail.com>
2018-11-12 16:04:40 +0100

    better intend

--- Platfromer 2D/ui/UIScaler.gd
+++ Platfromer 2D/ui/UIScaler.gd
@@ -5,6 +5,6 @@ func _ready():
 	globals.connect("window_size_changed", self, "_on_window_size_changed")
 
 func _on_window_size_changed(prev, now):
-		var width = globals.default_window_size.x
-		scale.x = now.x / width
-		scale.y = scale.x
+	var width = globals.default_window_size.x
+	scale.x = now.x / width
+	scale.y = scale.x
