9cc34c8c9826b4f6bf03078ab945ff67bb7b91be
Jeremi Biernacki <jeremi360@gmail.com>
2020-04-25 14:33:08 +0200

    reorgazine project

--- objects/liht/Liht.gd
+++ objects/liht/Liht.gd
@@ -4,6 +4,6 @@ func _ready() -> void:
 	globals.max_score += 1
 
 func _on_Liht_body_entered(body: PhysicsBody2D) -> void:
-	if body.name == "Player":
+	if body.name == "Hero":
 		globals.score += 1
 		queue_free()
